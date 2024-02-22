610d8c40e4845c8b6687cf7e6a60bba7779005c1
Jarlin Fonseca <jarlinandres5000@gmail.com>
2024-02-22 11:13:27 -0500

    Manejo de excepciones para autorización basada en métodos seguros

--- src/main/java/com/cursos/api/springsecuritycourse/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cursos/api/springsecuritycourse/exception/GlobalExceptionHandler.java
@@ -4,9 +4,11 @@ import com.cursos.api.springsecuritycourse.dto.ApiError;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
@@ -26,6 +28,22 @@ public class GlobalExceptionHandler {
 
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handlerAccessDeniedException(HttpServletRequest request,
+                                                          AccessDeniedException exception){
+
+        ApiError apiError = new ApiError();
+        apiError.setBackendMessage(exception.getLocalizedMessage());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMethod(request.getMethod());
+        apiError.setMessage("Acceso denegado. No tienes los permisos necesarios para acceder a esta función. " +
+                "Por favor, contacta al administrador si crees que esto es un error.");
+        apiError.setTimestamp(LocalDateTime.now() );
+
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(apiError);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
