54f16cb630272436639ed4d24477a73902ecbaa3
Jarlin <jarlinandres5000@gmail.com>
2024-03-22 10:27:39 -0500

    Creando AuthorizaciónManager personalizado: endpoints privados

--- src/main/java/com/cursos/api/springsecuritycourse/config/security/authorization/CustomAuthorizationManager.java
+++ src/main/java/com/cursos/api/springsecuritycourse/config/security/authorization/CustomAuthorizationManager.java
@@ -1,12 +1,18 @@
 package com.cursos.api.springsecuritycourse.config.security.authorization;
 
 
+import com.cursos.api.springsecuritycourse.exception.ObjectNotFoundException;
+import com.cursos.api.springsecuritycourse.persistence.entity.security.GrantedPermission;
 import com.cursos.api.springsecuritycourse.persistence.entity.security.Operation;
+import com.cursos.api.springsecuritycourse.persistence.entity.security.User;
 import com.cursos.api.springsecuritycourse.persistence.repository.security.OperationRepository;
+import com.cursos.api.springsecuritycourse.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.core.Authentication;
@@ -14,6 +20,7 @@ import org.springframework.security.web.access.intercept.RequestAuthorizationCon
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,6 +30,7 @@ import java.util.regex.Pattern;
 public class CustomAuthorizationManager implements AuthorizationManager<RequestAuthorizationContext> {
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomAuthorizationManager.class);
     private final OperationRepository operacionRepository;
+    private final UserService userService;
 
     @Override
     public AuthorizationDecision check(Supplier<Authentication> authentication,
@@ -34,21 +42,54 @@ public class CustomAuthorizationManager implements AuthorizationManager<RequestA
 
         boolean isPublic = isPublic(url, httpMethod);
 
+        if(isPublic){
+            return new AuthorizationDecision(true);
+        }
 
-        return new AuthorizationDecision(isPublic);
+
+        boolean isGranted = isGranted(url, httpMethod, authentication.get());
+
+        return new AuthorizationDecision(isGranted);
+    }
+
+    private boolean isGranted(String url, String httpMethod, Authentication authentication) {
+        if(!(authentication instanceof UsernamePasswordAuthenticationToken)){
+           throw new AuthenticationCredentialsNotFoundException("User not logged in");
+        }
+
+        List<Operation> operations = obtainedOperations(authentication);
+
+        boolean isGranted = operations.stream().anyMatch(getOperationPredicate(url, httpMethod));
+        LOGGER.info("IS GRANTED: {}",isGranted);
+        return isGranted;
+    }
+
+    private static Predicate<Operation> getOperationPredicate(String url, String httpMethod) {
+        return operation -> {
+            String basePath = operation.getModule().getBasePath();
+
+            Pattern pattern = Pattern.compile(basePath.concat(operation.getPath()));
+            Matcher matcher = pattern.matcher(url);
+            return matcher.matches() && operation.getHttpMethod().equals(httpMethod);
+        };
+    }
+
+    private List<Operation> obtainedOperations(Authentication authentication) {
+        UsernamePasswordAuthenticationToken authToken = (UsernamePasswordAuthenticationToken) authentication;
+        String username = (String) authToken.getPrincipal();
+        User user = userService.findOneByUsername(username)
+                .orElseThrow(()-> new ObjectNotFoundException("User not found: "+username));
+
+        return user.getRole().getPermissions().stream()
+                .map(GrantedPermission::getOperation)
+                .toList();
     }
 
     private boolean isPublic(String url, String httpMethod) {
         List<Operation> publicAccessEndpoints = operacionRepository
                 .findByPublicAccess();
 
-        boolean isPublic = publicAccessEndpoints.stream().anyMatch(operation -> {
-            String basePath = operation.getModule().getBasePath();
-
-            Pattern pattern = Pattern.compile(basePath.concat(operation.getPath()));
-            Matcher matcher = pattern.matcher(url);
-            return matcher.matches() && operation.getHttpMethod().equals(httpMethod);
-        });
+        boolean isPublic = publicAccessEndpoints.stream().anyMatch(getOperationPredicate(url, httpMethod));
         LOGGER.info("IS PUBLIC: {}",isPublic);
 
         return isPublic;
