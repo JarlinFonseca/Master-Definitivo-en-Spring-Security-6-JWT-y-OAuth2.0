4917d20cd906d150ba4ecd7986b3b91844385cad
Jarlin Fonseca <jarlinandres5000@gmail.com>
2024-03-25 18:13:16 -0500

    Configuración CORS para ambiente docker

--- src/main/java/com/cursos/api/springsecuritycourse/config/security/HttpSecurityConfig.java
+++ src/main/java/com/cursos/api/springsecuritycourse/config/security/HttpSecurityConfig.java
@@ -5,6 +5,7 @@ import com.cursos.api.springsecuritycourse.persistence.util.RoleEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authorization.AuthorizationManager;
@@ -64,10 +65,26 @@ public class HttpSecurityConfig {
 
     }
 
+    @Profile({"local", "dev"})
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    CorsConfigurationSource defaultCorsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList( "http://127.0.0.1:5500", "https://www.google.com"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+    @Profile("docker")
+    @Bean
+    CorsConfigurationSource dockerCorsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://client"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
