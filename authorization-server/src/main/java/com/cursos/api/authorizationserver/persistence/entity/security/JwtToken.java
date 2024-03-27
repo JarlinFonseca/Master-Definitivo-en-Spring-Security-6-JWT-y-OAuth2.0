dbfe498ae1b5e2031a38247ba763f2387e0e168d
Jarlin <jarlinandres5000@gmail.com>
2024-03-27 17:25:41 -0500

    Fin del curso y actualización de la colección de Postman

--- authorization-server/src/main/java/com/cursos/api/authorizationserver/persistence/entity/security/JwtToken.java
+++ authorization-server/src/main/java/com/cursos/api/authorizationserver/persistence/entity/security/JwtToken.java
@@ -1,7 +1,6 @@
 package com.cursos.api.authorizationserver.persistence.entity.security;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
--- spring-security-course/src/main/java/com/cursos/api/springsecuritycourse/config/security/ResourceServerHttpSecurityConfig.java
+++ spring-security-course/src/main/java/com/cursos/api/springsecuritycourse/config/security/ResourceServerHttpSecurityConfig.java
@@ -1,11 +1,9 @@
 package com.cursos.api.springsecuritycourse.config.security;
 
-import com.cursos.api.springsecuritycourse.config.security.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
